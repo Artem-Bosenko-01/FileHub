703ec3e7b7cbe853e67bae3500e36cd24920fe7d
Босенко Артем <artem.bosenko@nure.ua>
2021-04-29 09:47:14 +0300

    Add API for booby language

---
+++ calculator/app/src/main/java/com/teamdev/booby/BoobyAPI.java
@@ -0,0 +1,4 @@
+package com.teamdev.booby;
+
+public interface BoobyAPI {
+}
---
+++ calculator/app/src/main/java/com/teamdev/booby/impl/BoobyImpl.java
@@ -0,0 +1,6 @@
+package com.teamdev.booby.impl;
+
+import com.teamdev.booby.BoobyAPI;
+
+public class BoobyImpl implements BoobyAPI {
+}

ee983c211f86e668f9713913f03d66afeb078306
Босенко Артем <artem.bosenko@nure.ua>
2021-05-11 20:57:41 +0300

    Delete unused package.

--- math_calculator/app/src/main/java/com/teamdev/calculator/compiler/fsm/operand/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * This package contains implementation instruments to detect operand
- * in {@link com.teamdev.calculator.compiler.InputCharacterStream character stream}.
- * */
-package com.teamdev.calculator.compiler.fsm.operand;

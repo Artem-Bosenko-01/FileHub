29a54420e9c00e619d9d5b0ddea514069688c67b
Босенко Артем <artem.bosenko@nure.ua>
2021-06-30 23:10:06 +0300

    Add custom exception class for defined id element and error massage.

--- fileHub/js-app/app/index.js
+++ fileHub/js-app/app/index.js
@@ -1,3 +1,11 @@
+class ValidationError{
+
+    constructor(id, reason) {
+        this.id = id;
+        this.reason = reason;
+    }
+}
+
 document.getElementById("button").addEventListener('click', () => {
 
     const inputs = document.querySelectorAll(".input-value input");
@@ -8,47 +16,25 @@ document.getElementById("button").addEventListener('click', () => {
     const emailPromise = emailValidation(email);
     const passwordPromise = passwordValidation(password);
 
-    /*
-        const mainValidation = new Promise((resolve, reject) => {
-            resolve();
-        })
-
-            mainValidation.then(() => emailValidation(email), (reject) => drawErrorState("user-email", reject));
-            mainValidation.then(() => passwordValidation(password), (reject) => drawErrorState("user-password", reject));
-            mainValidation.then(()=>{
-                alert(email + "\n" + password)
-            })
-            mainValidation.catch()*/
-
-    /*    emailPromise.catch((reject) => {
-            drawErrorState("user-email", reject)
-        })
-
-        passwordPromise.catch((reject) => {
-            drawErrorState("user-password", reject)
-        })*/
 
     Promise.all([emailPromise, passwordPromise])
-        .then(() => alert(email + "\n" + password))
+        .then(() => alert("Email -> " + email + ".\nPassword -> " + password + "."))
         .catch(
-            (reason, id) => drawErrorState(id, reason)
+            (error) => drawErrorState(error.id, error.reason)
         );
 
 })
 
 
 function emailValidation(emailUser) {
-
     return new Promise((resolve, reject) => {
 
-        debugger
-
         if (emailUser.length < 5) {
-            reject(`Email length should be more than 4 symbols`, 'user-email');
+            reject(new ValidationError('user-email', `Email length should be more than 4 symbols`));
         }
 
         if (/^[a-zA-Z0-9+._@-]*$/.test(emailUser) === false) {
-            reject(`Email should be contains a-zA-Z, 0-9 or symbols like "+._@-"`, 'user-email');
+            reject(new ValidationError('user-email', `Email should be contains a-zA-Z, 0-9 or symbols like "+._@-"`));
         }
 
         resolve();
@@ -58,7 +44,7 @@ function emailValidation(emailUser) {
 function passwordValidation(passwordUser) {
     return new Promise((resolve, reject) => {
         if (passwordUser.length < 6) {
-            reject(`Password length should be more than 5 symbols`, 'user-password');
+            reject(new ValidationError('user-password', `Password length should be more than 5 symbols`));
         }
 
         resolve();

f2a9d15417ec689ede0a2325bbe8e91454b10be8
Босенко Артем <artem.bosenko@nure.ua>
2021-06-30 16:06:37 +0300

    Add event listener for form.

--- fileHub/js-app/app/index.js
+++ fileHub/js-app/app/index.js
@@ -1,26 +1,10 @@
-/*First method*/
-/*
-function info(){
-    let email = document.getElementById("email-user").value;
-    let password = document.getElementById("password-user").value;
+document.getElementById("form").addEventListener('submit', ()=>{
 
-    if(email ==="") email = "empty";
-    if(password ==="") password = "empty";
+    const inputs = document.querySelectorAll(".input-value input");
+
+    const email = inputs[0].value;
+    const password = inputs[1].value;
 
     alert("Email -> " + email + ".\nPassword -> " + password + ".");
-}
-*/
 
-/*Second method*/
-function info() {
-
-    let data = document.querySelectorAll(".input-value input");
-
-    let email = data[0].value;
-    let password = data[1].value;
-
-    if (email === "") email = "empty";
-    if (password === "") password = "empty";
-
-    alert("Email -> " + email + ".\nPassword -> " + password + ".");
-}
+});

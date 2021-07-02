8ba23eb1a3c26836a338bd5aba0b233058253345
artem bosenko <bosenko92.ab@gmail.com>
2021-07-02 11:49:12 +0300

    Add property shorthand for QUnit.

--- fileHub/js-app/tests/app/tests-index.js
+++ fileHub/js-app/tests/app/tests-index.js
@@ -11,8 +11,10 @@ const INVALID_PASSWORDS = ['1654', 'DAC', 'q']
 const CORRECT_PASSWORD = '123456'
 const INVALID_CONFIRM_PASSWORD = '123654'
 
-QUnit.module('should fail email length validation', () => {
-    QUnit.test('should check email length more than 4 symbols', (assert) => {
+const {module, test} = QUnit;
+
+module('should fail email length validation', () => {
+    test('should check email length more than 4 symbols', (assert) => {
 
         TOO_SHORT_EMAILS.forEach(
             email => assert.rejects(emailLengthValidation(email), `Invalid value -> ${email}`)
@@ -20,8 +22,8 @@ QUnit.module('should fail email length validation', () => {
     });
 });
 
-QUnit.module('should fail email structure validation', () => {
-    QUnit.test('should check email contains only that symbols: a-zA-Z 0-9 +.-_@', (assert) => {
+module('should fail email structure validation', () => {
+    test('should check email contains only that symbols: a-zA-Z 0-9 +.-_@', (assert) => {
 
         INVALID_STRUCTURE_EMAILS.forEach(
             email => assert.rejects(emailStructureValidation(email), `Invalid value -> ${email}`)
@@ -29,16 +31,16 @@ QUnit.module('should fail email structure validation', () => {
     });
 });
 
-QUnit.module('should fail password structure validation', () => {
-    QUnit.test('should check password length more than 5 symbols', (assert) => {
+module('should fail password structure validation', () => {
+    test('should check password length more than 5 symbols', (assert) => {
         INVALID_PASSWORDS.forEach(
             password => assert.rejects(passwordValidation(password), `Invalid value -> ${password}`)
         )
     });
 });
 
-QUnit.module('should fail confirm password validation', () => {
-    QUnit.test('should check that two passwords are equals', (assert) => {
+module('should fail confirm password validation', () => {
+    test('should check that two passwords are equals', (assert) => {
         assert.rejects(confirmPasswordValidation(CORRECT_PASSWORD, INVALID_CONFIRM_PASSWORD),
             `${CORRECT_PASSWORD} not equal ${INVALID_CONFIRM_PASSWORD}`)
     });

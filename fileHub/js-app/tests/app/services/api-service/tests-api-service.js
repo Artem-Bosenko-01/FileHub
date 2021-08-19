feb346eb8731bb62b37a5db5dc2a70bb4a0c9ae4
artem bosenko <artem.bosenko@teamdev.com>
2021-08-19 11:19:20 +0300

    Add test for apiService method: onNavigateAfterError

--- fileHub/js-app/tests/app/services/api-service/tests-api-service.js
+++ fileHub/js-app/tests/app/services/api-service/tests-api-service.js
@@ -1,8 +1,21 @@
 import testsAuthenticationApiService from '../../login/tests-authentication-api-service.js';
 import testsRegistrationApiService from '../../register/tests-registration-api-service.js';
-const {module} = QUnit;
+import {ApiService} from '../../../../app/services/api-service/api-service.js';
+
+const {module, test} = QUnit;
 
 module('APIService', () => {
   testsAuthenticationApiService();
   testsRegistrationApiService();
+
+  test('Should add and call listener on 401 response code status.', (assert) => {
+    const get401ResponseStatusStep = '401 response status';
+    const mockWindow = new EventTarget();
+    const apiService = new ApiService(mockWindow);
+    apiService.onNavigateAfterError(assert.step(get401ResponseStatusStep));
+
+    mockWindow.dispatchEvent(new CustomEvent('unauthorizedUserError'));
+
+    assert.verifySteps([get401ResponseStatusStep]);
+  });
 });

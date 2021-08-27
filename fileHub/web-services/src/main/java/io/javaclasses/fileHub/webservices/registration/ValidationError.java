7a2de7ccad960dae6755a7ca049b5db3fa44af61
Босенко Артем <artem.bosenko@teamdev.com>
2021-08-27 13:01:21 +0300

    Rename ValidationError fields.

--- fileHub/web-services/src/main/java/io/javaclasses/fileHub/webservices/registration/ValidationError.java
+++ fileHub/web-services/src/main/java/io/javaclasses/fileHub/webservices/registration/ValidationError.java
@@ -4,10 +4,10 @@ package io.javaclasses.fileHub.webservices.registration;
  * Type of error in the registration user process.
  */
 public class ValidationError {
-    private final String fieldName;
-    private final String errorMessage;
+    private final String field;
+    private final String message;
     public ValidationError(String fieldName, String errorMessage){
-        this.fieldName = fieldName;
-        this.errorMessage = errorMessage;
+        this.field = fieldName;
+        this.message = errorMessage;
     }
 }

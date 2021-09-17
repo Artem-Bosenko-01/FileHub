c8d798d3604826b28dc10f2f10dc036154ad6ff9
Босенко Артем <artem.bosenko@teamdev.com>
2021-09-17 13:50:29 +0300

    Refactor javadoc for SpringConfig.

--- fileHub/web-services/src/main/java/io/javaclasses/fileHub/webservices/SpringConfig.java
+++ fileHub/web-services/src/main/java/io/javaclasses/fileHub/webservices/SpringConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * Configuration of Spring application.
+ * Configuration of Spring framework for the FileHb application.
  */
 @Configuration
 @ComponentScan("io.javaclasses.fileHub")

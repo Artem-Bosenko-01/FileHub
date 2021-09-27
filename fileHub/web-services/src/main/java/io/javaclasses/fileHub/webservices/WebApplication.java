1d48e464cf0fb25bc5561c86ae790be72bd5d9d7
Босенко Артем <artem.bosenko@teamdev.com>
2021-09-27 12:53:30 +0300

    Improve code readability.

--- fileHub/web-services/src/main/java/io/javaclasses/fileHub/webservices/WebApplication.java
+++ fileHub/web-services/src/main/java/io/javaclasses/fileHub/webservices/WebApplication.java
@@ -1,25 +1,35 @@
 package io.javaclasses.fileHub.webservices;
 
-import io.javaclasses.fileHub.webservices.filesystem.*;
+import io.javaclasses.fileHub.webservices.filesystem.UploadFileRoute;
+import io.javaclasses.fileHub.webservices.filesystem.DownloadFileRoute;
+import io.javaclasses.fileHub.webservices.filesystem.DeleteFileRoute;
+import io.javaclasses.fileHub.webservices.filesystem.CreateFolderRoute;
+import io.javaclasses.fileHub.webservices.filesystem.GetFolderByIdRoute;
+import io.javaclasses.fileHub.webservices.filesystem.GetRootFolderRoute;
+import io.javaclasses.fileHub.webservices.filesystem.UpdateFileRoute;
+import io.javaclasses.fileHub.webservices.filesystem.GetFolderContentRoute;
+import io.javaclasses.fileHub.webservices.filesystem.DeleteFolderRoute;
+import io.javaclasses.fileHub.webservices.filesystem.UpdateFolderRoute;
 import io.javaclasses.fileHub.webservices.user.AuthenticationRoute;
 import io.javaclasses.fileHub.webservices.user.GetUserInfoRoute;
 import io.javaclasses.fileHub.webservices.user.LogOutRoute;
 import io.javaclasses.fileHub.webservices.user.RegistrationRoute;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import spark.Service;
 import spark.Spark;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import static spark.Service.ignite;
 import static spark.Spark.*;
 
 /**
  * Configures and starts server for FileHub application. Sets main existed paths for routing at FileHub application.
  */
 public final class WebApplication {
+
+    private static final String APPLICATION_NAME = "/FileHub/server/api";
+    private static final String API_VERSION_1_0 = "/1.0";
 
     static {
 
@@ -39,27 +49,37 @@ public final class WebApplication {
 
         ServiceLocator service = applicationContext.getBean("serviceLocator", ServiceLocator.class);
 
-        String APPLICATION_NAME = "/FileHub/server/api";
-        String API_VERSION_1_0 = "/1.0";
+        initRoutesForFilesSystem(service);
 
-        post(APPLICATION_NAME + API_VERSION_1_0 + "/login", new AuthenticationRoute(service.authenticateUser()));
-        post(APPLICATION_NAME + API_VERSION_1_0 + "/register", new RegistrationRoute(service.registerUser()));
-        get(APPLICATION_NAME + API_VERSION_1_0 + "/root-folder", new GetRootFolderRoute(service.getRootFolder()));
-        get(APPLICATION_NAME + API_VERSION_1_0 + "/folder/:id", new GetFolderByIdRoute(service.getFolderById()));
-        get(APPLICATION_NAME + API_VERSION_1_0 + "/user", new GetUserInfoRoute(service.getUser()));
-        get(APPLICATION_NAME + API_VERSION_1_0 + "/folder/:id/content", new GetFolderContentRoute(service.getFolderContent()));
-        delete(APPLICATION_NAME + API_VERSION_1_0 + "/folder/:id", new DeleteFolderRoute(service.deleteFolder()));
-        delete(APPLICATION_NAME + API_VERSION_1_0 + "/file/:id", new DeleteFileRoute(service.deleteFile()));
-        post(APPLICATION_NAME + API_VERSION_1_0 + "/folder/:id/folder", new CreateFolderRoute(service.createFolder()));
-        post(APPLICATION_NAME + API_VERSION_1_0 + "/logOut", new LogOutRoute(service.logOut()));
-        put(APPLICATION_NAME + API_VERSION_1_0 + "/folder/:id", new UpdateFolderRoute(service.updateFolder()));
-        put(APPLICATION_NAME + API_VERSION_1_0 + "/file/:id", new UpdateFileRoute(service.updateFile()));
-        post(APPLICATION_NAME + API_VERSION_1_0 + "/folder/:id/file", new UploadFileRoute(service.uploadFile()));
-        get(APPLICATION_NAME + API_VERSION_1_0 + "/file/:id", new DownloadFileRoute(service.getFileContent()));
+        initRoutesForUserSystem(service);
     }
 
     public void stop() {
 
         Spark.stop();
+    }
+
+    private void initRoutesForUserSystem(ServiceLocator service) {
+
+        post(APPLICATION_NAME + API_VERSION_1_0 + "/login", new AuthenticationRoute(service.authenticateUser()));
+        post(APPLICATION_NAME + API_VERSION_1_0 + "/register", new RegistrationRoute(service.registerUser()));
+        get(APPLICATION_NAME + API_VERSION_1_0 + "/user", new GetUserInfoRoute(service.getUser()));
+        post(APPLICATION_NAME + API_VERSION_1_0 + "/logOut", new LogOutRoute(service.logOut()));
+
+    }
+
+    private void initRoutesForFilesSystem(ServiceLocator service) {
+
+        get(APPLICATION_NAME + API_VERSION_1_0 + "/root-folder", new GetRootFolderRoute(service.getRootFolder()));
+        get(APPLICATION_NAME + API_VERSION_1_0 + "/folder/:id", new GetFolderByIdRoute(service.getFolderById()));
+        get(APPLICATION_NAME + API_VERSION_1_0 + "/folder/:id/content", new GetFolderContentRoute(service.getFolderContent()));
+        delete(APPLICATION_NAME + API_VERSION_1_0 + "/folder/:id", new DeleteFolderRoute(service.deleteFolder()));
+        delete(APPLICATION_NAME + API_VERSION_1_0 + "/file/:id", new DeleteFileRoute(service.deleteFile()));
+        post(APPLICATION_NAME + API_VERSION_1_0 + "/folder/:id/folder", new CreateFolderRoute(service.createFolder()));
+        put(APPLICATION_NAME + API_VERSION_1_0 + "/folder/:id", new UpdateFolderRoute(service.updateFolder()));
+        put(APPLICATION_NAME + API_VERSION_1_0 + "/file/:id", new UpdateFileRoute(service.updateFile()));
+        post(APPLICATION_NAME + API_VERSION_1_0 + "/folder/:id/file", new UploadFileRoute(service.uploadFile()));
+        get(APPLICATION_NAME + API_VERSION_1_0 + "/file/:id", new DownloadFileRoute(service.getFileContent()));
+
     }
 }
